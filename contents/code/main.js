c8badc3ee50787dee9fd19c3ac5b6c181c91efdd
Sqaaakoi <sqaaakoi@sqaaakoi.xyz>
2023-01-24 02:49:31 +1300

    add a readme, document code, fix typo

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,3 +1,5 @@
+// client.activities being empty means shown on all activities
+
 function windowActivityPin() {
     let client = workspace.activeClient;
     if (!client) return;
@@ -5,6 +7,7 @@ function windowActivityPin() {
         // Window is already pinned to all activities; pin it to only here
         client.activities = [workspace.currentActivity];
     } else {
+        // Show the window on all activities
         client.activities = [];
     }
 }
@@ -13,10 +16,12 @@ function windowActivityUnpin() {
     let client = workspace.activeClient;
     if (!client) return;
     if (client.activities.length == 0) {
+        // client.activities is empty; and logically can be considered as all of the activities.
         client.activities = workspace.activities.filter(function(activity) {
             return activity != workspace.currentActivity;
         });
     } else {
+        // If this is the only activity that the window is shown on, this will show the window on all activities
         client.activities = client.activities.filter(function(activity) {
             return activity != workspace.currentActivity;
         });
